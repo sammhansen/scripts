f7a777da405f8ad8b16132f062a218d158945ebf
Hansen <samhansen.dev@gmail.com>
2025-05-08 15:35:30 +0300

    fix: drop all states while charging

--- batt-warns.sh
+++ batt-warns.sh
@@ -1,34 +1,47 @@
-# wrote this because my laptop keeps dying on me everytime. You should poll this script with sth like waybar.
+# wrote this because my laptop keeps dying on me all the time. You should poll this script with sth like waybar.
 
-USERNAME="atlantis"
+USERNAME="t4m4r4"
+DOTS_PATH="atlantis"
+ICONS_PATH="/home/$USERNAME/$DOTS_PATH/.local/share/assets/icons"
 LOW=15
-CRITICAL=7
+CRITICAL=5
 
-WARN_FILE=/tmp/.batt-warn-sent
+WARN_CRITICAL_FILE="/tmp/.batt-warn-critical"
+WARN_LOW_FILE="/tmp/.batt-warn-low"
+WARN_FULL_FILE="/tmp/.batt-warn-full"
+
 BATT_STATUS=$(cat /sys/class/power_supply/BAT1/status)
 BATT_LEVEL=$(cat /sys/class/power_supply/BAT1/capacity)
 
 if [ "$BATT_STATUS" = "Discharging" ]; then
   if [ "$BATT_LEVEL" -le "$CRITICAL" ]; then
-    if [ ! -f "$WARN_FILE" ]; then
-      notify-send "System" "Battery critical.Suspending in 5 seconds" -i /home/"$USERNAME"/.local/share/assets/icons/nix.png -u normal
+    if [ ! -f "$WARN_CRITICAL_FILE" ]; then
+      notify-send "Battery critical" "Suspending in 5 seconds" -i "$ICONS_PATH"/batt-critical.png -u normal
       sleep 5
       systemctl suspend
-      touch "$WARN_FILE"
+      touch "$WARN_CRITICAL_FILE"
     fi
   elif [ "$BATT_LEVEL" -le "$LOW" ]; then
-    if [ ! -f "$WARN_FILE" ]; then
-      notify-send "System" "Battery low.Please plug me in kudasai" -i /home/"$USERNAME"/.local/share/assets/icons/nix.png -u normal
-      touch "$WARN_FILE"
+    if [ ! -f "$WARN_LOW_FILE" ]; then
+      notify-send "Battery low" "Please plug me in kudasai" -i "$ICONS_PATH"/batt-low.png -u normal
+      touch "$WARN_LOW_FILE"
     fi
   else
     :
   fi
+
+  rm "$WARN_FULL_FILE"
+
 elif [ "$BATT_STATUS" = "Charging" ]; then
-  rm -f "$WARN_FILE"
+  rm -f "$WARN_CRITICAL_FILE"
+  rm -f "$WARN_LOW_FILE"
+  rm -f "$WARN_FULL_FILE"
+
 elif [ "$BATT_STATUS" = "Full" ]; then
-  notify-send "System" "Battery full.Please unplug me" -i /home/"$USERNAME"/.local/share/assets/icons/nix.png -u normal
-  rm -f "$WARN_FILE"
+  if [ ! -f "$WARN_FULL_FILE" ]; then
+    notify-send "Battery full" "Please unplug me" -i "$ICONS_PATH"/batt-full.png -u normal
+    touch "$WARN_FULL_FILE"
+  fi
 else
   :
 fi
